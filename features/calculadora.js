a2281fe4e0fccfd2c65643041fb0a280f26d14f3
Mariana Suarez <mariana.suarezh97@gmail.com>
2026-02-25 13:01:29 -0500

    feat: agregamos ejemplos como prueba de las funciones suma y resta

--- features/calculadora.js
+++ features/calculadora.js
@@ -7,3 +7,6 @@ function suma(numero1, numero2) {
 function resta(numero1, numero2) {
     return numero1 - numero2;
 }
+
+console.log(suma(5, 3)); // ejemplo suma: 8
+console.log(resta(5, 3)); // ejemplo resta: 2
